1d51a87d209f0437d90b3410e2a6745fc3a6d50b
Daemon <makushin.dima@gmail.com>
2020-10-05 10:36:25 +0300

    up:

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { ShareModule } from "../../share/share.module";
 import { HomeComponent } from "./home.component";
 
 const routes: Routes = [
@@ -15,9 +13,7 @@ const routes: Routes = [
   declarations: [],
   exports: [RouterModule],
   imports: [
-    // CommonModule,
     RouterModule.forChild(routes),
-    // ShareModule
   ]
 })
 export class HomeModule { }
--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -2,23 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { MenuComponent } from './menu/menu.component';
-import { SlidebarComponent } from './slidebar/slidebar.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     FooterComponent,
-    HeaderComponent,
-    MenuComponent,
-    SlidebarComponent
+    HeaderComponent
   ],
   exports: [
     HeaderComponent,
-    FooterComponent,
-    MenuComponent,
-    SlidebarComponent
+    FooterComponent
   ],
   imports: [
     CommonModule,
